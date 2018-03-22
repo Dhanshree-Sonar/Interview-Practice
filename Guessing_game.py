adfb85ede1a64d31186e83740eeff35c724eeffc
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-21 23:53:59 -0400

    Refactor: Explain the use of the imported module

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,6 +1,8 @@
 ##Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number,
 ##then tell them whether they guessed too low, too high, or exactly right.
 
+# Used random module to generate random number
+
 import random
 
 def guessing_game():
