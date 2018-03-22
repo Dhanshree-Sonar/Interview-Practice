2efdb44e3b08097f153e983c4d75dc2794414eee
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-21 23:44:33 -0400

    Feat: Add logic to generate password
    This code is return to practice python coding.

---
+++ password_generator.py
@@ -0,0 +1,20 @@
+##Write a password generator in Python. Be creative with how you generate
+##passwords - strong passwords have a mix of lowercase letters, uppercase letters,
+##numbers, and symbols. The passwords should be random, generating a new password
+##every time the user asks for a new password.
+
+import string
+import random
+
+def generate_password(strength):
+    option = list(string.ascii_letters) + list(string.digits) + list(string.punctuation)
+
+    length = 16 if strength == 'strong' else 8
+
+    return "".join(random.sample(option, length))
+
+password = generate_password('strong')
+print (password)
+
+password = generate_password('short')
+print (password)
