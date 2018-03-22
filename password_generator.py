4b835fe41285b11472b87f61a642067f644ff756
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-21 23:52:38 -0400

    Refactor: Explain the use of the imported modules

--- password_generator.py
+++ password_generator.py
@@ -3,6 +3,9 @@
 ##numbers, and symbols. The passwords should be random, generating a new password
 ##every time the user asks for a new password.
 
+# Used random to generate random numbers
+# Used string module to get letters digits and special characters
+
 import string
 import random
 
@@ -14,6 +17,9 @@ def generate_password(strength):
     return "".join(random.sample(option, length))
 
 password = generate_password('strong')
+print (password)
+
+password = generate_password('short')
 print (password)
 
 password = generate_password('short')
