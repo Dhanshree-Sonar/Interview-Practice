da18e93c777faf6e074006e8addf93264e1656c9
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-22 23:39:47 -0400

    Refactor: Explain complexity and input output

--- max_tip.py
+++ max_tip.py
@@ -8,4 +8,17 @@ max_tip = 0
 for i in range(int(n)):
     max_tip += int(Ai[i]) if int(Ai[i])>int(Bi[i]) else int(Bi[i])
 
-print "Max Tip: " + str(max_tip)    
+print "Max Tip: " + str(max_tip)
+
+##Complexity:
+##    Space: O(1), need just one counter to store max tip
+##    Time: O(N) where N is number of orders
+
+##Input:
+##5 3 3
+##1 2 3 4 5
+##5 4 3 2 1
+##Output:
+##21
+
+
