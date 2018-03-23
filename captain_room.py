97f8a6a42cbc14c487d44634e05b81f3e43e3469
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-03-22 21:30:24 -0400

    Feat: Add logic to find captain's room number
    This code is written to practice python sets.

---
+++ captain_room.py
@@ -0,0 +1,17 @@
+def find_captain_room(k, rooms):
+##    room_nums = set(rooms)
+##    for room in room_nums:
+##        if rooms.count(room) == 1:
+##            return room
+    # Multiply all the unique room by group number including captain's room
+    # Minus the sum of room so we have (k-1)*captain's room
+    # Divide result by (k-1) to get captain's room number
+    print ((sum(set(rooms))*5) - (sum(rooms)))/(k-1)
+
+k = 5
+rooms = [1, 2, 3, 6, 5, 4, 4, 2, 5, 3, 6, 1, 6, 5, 3, 2, 4, 1, 2, 5, 1, 4,
+         3, 6, 8, 4, 3, 1, 5, 6, 2]
+
+find_captain_room(k, rooms)
+
+
