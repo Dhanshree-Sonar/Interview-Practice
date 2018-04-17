f901ec8d7bf372303346c75c22365bbd29c4c56e
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-04-16 23:21:04 -0400

    Docs: Add problem statements for all questions

--- solutions.py
+++ solutions.py
@@ -1,5 +1,11 @@
 import unittest
 
+##Question 1
+##Given two strings s and t, determine whether some anagram of t is a substring
+##of s. For example: if s = "udacity" and t = "ad", then the function returns
+##True. Your function definition should look like: question1(s, t) and return
+##a boolean True or False.
+
 def question1(s,t):
     # Check whether arguements are of string data-type
     if type(s) != str or type(t) != str:
@@ -46,6 +52,10 @@ class question1TestCase(unittest.TestCase):
         self.assertEqual(question1("udacity", 1), "Error: Argument(s) not string")
 
 
+##Question 2
+##Given a string a, find the longest palindromic substring contained in a.
+##Your function definition should look like question2(a), and return a string.
+
 def question2(a):
     # Check whether arguement is of string data-type
     if type(a) != str:
@@ -84,6 +94,19 @@ class question2TestCase(unittest.TestCase):
     def test_string_case_insensitive(self):
         self.assertEqual(question2('asRacecarde'), 'racecar')
 
+
+##Question 3
+##Given an undirected graph G, find the minimum spanning tree within G.
+##A minimum spanning tree connects all vertices in a graph with the smallest
+##possible total weight of edges. Your function should take in and return an
+##adjacency list structured like this:
+##
+##{'A': [('B', 2)],
+## 'B': [('A', 2), ('C', 5)], 
+## 'C': [('B', 5)]}
+##
+##Vertices are represented as unique strings.
+##The function definition should be question3(G)
 
 def question3(G):
 
@@ -203,6 +226,29 @@ class question3TestCase(unittest.TestCase):
         self.assertEqual(question3({}), {})
 
 
+##Question 4
+##Find the least common ancestor between two nodes on a binary search tree.
+##The least common ancestor is the farthest node from the root that is an ancestor
+##of both nodes. For example, the root is a common ancestor of all nodes on the
+##tree, but if both nodes are descendents of the root's left child,then that left
+##child might be the lowest common ancestor. You can assume that both nodes are
+##in the tree, and the tree itself adheres to all BST properties. The function
+##definition should look like question4(T, r, n1, n2), where T is the tree represe
+##nted as a matrix, where the index of the list is equal to the integer stored in
+##that node and a 1 represents a child node, r is a non-negative integer represent
+##ing the root, and n1 and n2 are non-negative integers representing the two nodes
+##in no particular order. For example, one test case might be
+##
+##question4([[0, 1, 0, 0, 0],
+##           [0, 0, 0, 0, 0],
+##           [0, 0, 0, 0, 0],
+##           [1, 0, 0, 0, 1],
+##           [0, 0, 0, 0, 0]],
+##          3,
+##          1,
+##          4)
+##and the answer would be 3.
+
 def question4(T, r, n1, n2):
     # Check if T is not a set
     if type(T) != list:
@@ -297,6 +343,19 @@ class question4TestCase(unittest.TestCase):
     def test_node_greater_than_root(self):
         self.assertEqual(question4(T,3,1,5), 'Error: Nodes can not be greater than largest tree element')
 
+
+##Question 5
+##Find the element in a singly linked list that's m elements from the end.
+##For example, if a linked list has 5 elements, the 3rd element from the end is
+##the 3rd element. The function definition should look like question5(ll, m),
+##where ll is the first node of a linked list and m is the "mth number from the
+##end". You should copy/paste the Node class below to use as a representation of
+##a node in the linked list. Return the value of the node at that position.
+##
+##class Node(object):
+##  def __init__(self, data):
+##    self.data = data
+##    self.next = None
 
 def question5(ll, m):
     # Check if m is an integer
